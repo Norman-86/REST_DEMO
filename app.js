5632fc11ad36f5fa751d9e810789b96e59852fcb
Park <normanpark85@outlook.com>
2021-05-14 15:40:08 +0300

    update PUT route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const posts = require('./posts.json')
 const fs = require('fs');
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 // fetch all posts
 app.get('/posts', (req, res) => {
     return res.json({posts})
@@ -38,12 +38,18 @@ app.post('/posts', (req, res) => {
 //update a single post
 app.put('/posts/:id', (req, res) => {
     let { id } = req.params;
-    let newPostText = req.body.post;
-    let foundPost = posts.find(post => posts.id === id)
-    if (foundPost) {
-        return foundPost.post = newPostText;
-    }
-    return res.status(200).json({ message: 'post has been edited' })
+    let newPostText = req.body.body;
+    let foundPost = posts.find(post => { return String(post.id) === id })
+    const index = posts.indexOf(foundPost)
+    posts[index].body = newPostText;
+    posts[index].title = req.body.title;
+    let stringedData = JSON.stringify(posts, null, 2)
+    fs.writeFile('posts.json', stringedData, function (err) {
+        if (err) {
+            return res.status(500).json({ message: err })
+        }
+    })
+ return res.status(200).json({ message: 'post has been edited' })
 });
 
 app.listen(3000, () => {
